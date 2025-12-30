e7d2604eb6b0ae84144400f88373d4d8c07db607
Cleiber Silva <cleibersilva@hotmail.com>
2025-12-30 17:02:21 -0300

    Atualiza mensagem para Versao 2.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
-    print("Hello from hello-home!")
-
+    print("Hello from hello-home! Versao 2.0")
+    print("Hello from hello-home! Versao 2.0")
 
 if __name__ == "__main__":
     main()
